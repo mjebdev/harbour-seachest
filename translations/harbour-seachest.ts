2c9f918e4c219cd8946e25713918587f57ee5766
Michael Barrett <69733844+mjebdev@users.noreply.github.com>
2025-01-19 17:39:41 +0000

    Update harbour-seachest.ts
    Minor edit to improve grammar.

--- translations/harbour-seachest.ts
+++ translations/harbour-seachest.ts
@@ -219,7 +219,7 @@ Italian translation by Legacychimera247</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Save files to the Downloads folder</source>
+        <source>Save files in the Downloads folder</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
