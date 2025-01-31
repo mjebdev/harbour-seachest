75208e2ecc3f5e7da95b5458aad503d0873176a2
legacychimera247 <mad_fer_it@vivaldi.net>
2025-01-31 12:06:53 +0100

    Add files via upload

---
+++ translations/harbour-seachest-it.ts
@@ -0,0 +1,285 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>An unofficial Dropbox client for Sailfish OS.
+
+by Michael J. Barrett
+
+Version 0.4
+Licensed under GNU GPLv3
+
+Swedish translation by Åke Engelbrektson
+
+Italian translation by Legacychimera247</source>
+        <translation>Un client non ufficiale di Dropbox per Sailfish OS.
+
+sviluppato da Michael J. Barrett
+
+Versione 0.4
+Sotto licenza GNU GPLv3
+
+Traduzione svedese di Åke Engelbrektson
+
+Traduzione italiana di Legacychimera247</translation>
+    </message>
+</context>
+<context>
+    <name>Authorize</name>
+    <message>
+        <source>To begin using the app, please authorize it to access your Dropbox account. This will open a new browser window.</source>
+        <translation>Per iniziare ad usare l&apos;app, per favore autorizza l&apos;accesso al tuo account Dropbox. Questo farà aprire una nuova finestra.</translation>
+    </message>
+    <message>
+        <source>Authorize</source>
+        <translation>Autorizza</translation>
+    </message>
+    <message>
+        <source>Authorization Successful</source>
+        <translation>Autorizzazione Riuscita</translation>
+    </message>
+</context>
+<context>
+    <name>Home</name>
+    <message>
+        <source>Error reauthorizing. Please try resubmitting your request.</source>
+        <translation>Errore nella re-autorizzazione. Per favore riprova.</translation>
+    </message>
+    <message>
+        <source>Error - Conflict with an existing folder path. Please choose a different name and try again.</source>
+        <translation>Errore - Conflitto con un percorso già esistente. Per favore scegli un nome diverso e riprova.</translation>
+    </message>
+    <message>
+        <source>Error creating folder - Code %1</source>
+        <translation>Errore nel creare la cartella - Codice %1</translation>
+    </message>
+    <message>
+        <source>Bad input parameter. Error copied.</source>
+        <translation>Paramentro input sbagliato. Errore copiato.</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Ricarica</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Pagina iniziale</translation>
+    </message>
+    <message>
+        <source>Create Folder</source>
+        <translation>Crea Cartella</translation>
+    </message>
+    <message>
+        <source>Upload in progress</source>
+        <translation>Caricamento in corso</translation>
+    </message>
+    <message>
+        <source>Upload</source>
+        <translation>Caricamento</translation>
+    </message>
+    <message>
+        <source>Error refreshing token: </source>
+        <translation>Errore nel ricaricare il token: </translation>
+    </message>
+    <message>
+        <source>Access to this feature was denied.</source>
+        <translation>L&apos;accesso a questa funzione è stato negato.</translation>
+    </message>
+    <message>
+        <source>Endpoint-specific error. Error copied to clipboard.</source>
+        <translation>Errore specifico dell&apos;endpoint. Errore copiato negli appunti.</translation>
+    </message>
+    <message>
+        <source>Too many requests.</source>
+        <translation>Troppe richieste.</translation>
+    </message>
+    <message>
+        <source>Error saving thumbnail to cache folder.</source>
+        <translation>Errore salvataggio anteprima nella cache.</translation>
+    </message>
+    <message>
+        <source>Modified on server:</source>
+        <translation>Modificato sul server:</translation>
+    </message>
+    <message>
+        <source>Modified on client:</source>
+        <translation>Modificato sul client:</translation>
+    </message>
+    <message>
+        <source>Size:</source>
+        <translation>Dimensione:</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Apri</translation>
+    </message>
+    <message>
+        <source>Download ZIP</source>
+        <translation>Scarica ZIP</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation>Scarica</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Rename</translation>
+    </message>
+    <message>
+        <source>Create Link</source>
+        <translation>Crea Collegamento</translation>
+    </message>
+    <message>
+        <source>Delete Folder</source>
+        <translation>Elimina cartella</translation>
+    </message>
+    <message>
+        <source>Delete File</source>
+        <translation>Elimina file</translation>
+    </message>
+    <message>
+        <source>4-hour link copied to clipboard.</source>
+        <translation>Collegamento valido 4 ore copiato negli appunti.</translation>
+    </message>
+    <message>
+        <source>Unable to rename item. Please try again &amp; avoid entering the same name.</source>
+        <translation>Impossibile rinminare l&apos;oggetto. Per favore riprova e prova ad inserire un altro nome.</translation>
+    </message>
+    <message>
+        <source>Unable to delete item. Error code 409.</source>
+        <translation>Impossibile eliminare l&apos;oggetto. Codice di errore 409.</translation>
+    </message>
+    <message>
+        <source>Unable to create link. Error code 409.</source>
+        <translation>Impossibile creare il collegamento. Codice di errore 409.</translation>
+    </message>
+    <message>
+        <source>Unexpected error - Code </source>
+        <translation>Errore inaspettato - Codice </translation>
+    </message>
+    <message>
+        <source>A file with the name &quot;%1&quot; already exists in the destination folder.</source>
+        <translation>Un file con il nome &quot;%1&quot; esiste già nella cartella di destinazione.</translation>
+    </message>
+    <message>
+        <source>Overwrite?</source>
+        <translation>Sovrascrivi?</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <source>Untitled</source>
+        <translation>Senza titolo</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Interface</source>
+        <translation>Interfaccia</translation>
+    </message>
+    <message>
+        <source>Action to download file</source>
+        <translation>Azione per scaricare il file</translation>
+    </message>
+    <message>
+        <source>Press &amp; hold for all options</source>
+        <translation>Premi e tieni premuto per tutte le opzioni</translation>
+    </message>
+    <message>
+        <source>Tap for all options</source>
+        <translation>Prremi per tutte le opzioni</translation>
+    </message>
+    <message>
+        <source>Tap</source>
+        <translation>Premi</translation>
+    </message>
+    <message>
+        <source>Press &amp; hold</source>
+        <translation>Tieni premuto</translation>
+    </message>
+    <message>
+        <source>Downloads</source>
+        <translation>Scaricamenti</translation>
+    </message>
+    <message>
+        <source>Show warning prior to overwriting a file on device</source>
+        <translation>Mostra un&apos;avvertenza prima di sovrascrivere un file sul dispositivo</translation>
+    </message>
+    <message>
+        <source>Save files in the Downloads folder</source>
+        <translation>Salva file nella cartella degli scaricamenti</translation>
+    </message>
+    <message>
+        <source>Currently set to </source>
+        <translation>Attualmente impostato su </translation>
+    </message>
+    <message>
+        <source>Authorization</source>
+        <translation>Autorizzazione</translation>
+    </message>
+    <message>
+        <source>Erase Access Key</source>
+        <translation>Elimina Chiave D&apos;accesso</translation>
+    </message>
+    <message>
+        <source>Home</source>
+        <translation>Pagina iniziale</translation>
+    </message>
+    <message>
+        <source>Save files in</source>
+        <translation>Salva file in</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-seachest</name>
+    <message>
+        <source>Home</source>
+        <translation>Pagina iniziale</translation>
+    </message>
+    <message>
+        <source>Error reauthorizing. Please try submitting request again.</source>
+        <translation>Errore nella re-autorizzazione. Per favore riprova.</translation>
+    </message>
+    <message>
+        <source>Error - File does not exist.</source>
+        <translation>Errore - Il file non esiste.</translation>
+    </message>
+    <message>
+        <source>Error - Unable to open file.</source>
+        <translation>Errore - Impossibile aprire il file.</translation>
+    </message>
+    <message>
+        <source>Other error: %1. Copied to clipboard.</source>
+        <translation>Altro errore: %1. Copiato negli appunti.</translation>
+    </message>
+    <message>
+        <source>Uploading </source>
+        <translation>Caricamento </translation>
+    </message>
+    <message>
+        <source>Downloading </source>
+        <translation>Scaricamento </translation>
+    </message>
+</context>
+</TS>
